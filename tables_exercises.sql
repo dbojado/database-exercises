f73164743d7c98d3312b7050fca9849894fd2fde
Daniella Bojado <daniella.bojado@gmail.com>
2020-07-20 15:46:22 -0500

    modified with corrections

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,21 +1,13 @@
-#Specify a Database to use 
 USE employees;
-
-#Shows the current database
-select database();
-
-#Shows any tables you have permission to see on the current database
-show tables;
-
-#Shows all the column names and data-types and contraints for a given table 
-DESCRIBE employees;
-
-#Shows the SQL that originally ran to generate that table 
-SHOW CREATE TABLE employees;
+SHOW tables;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE salaries;
+SHOW CREATE TABLE salaries;
 
 #What is the relationship between the employees and the departments tables?
-#they're both tables under the 'employees' database
-DESCRIBE departments;
+#they're both tables under the 'employees' database, other than that no direct relationship 
+#however dept_emp and dept_manager table do show a relationship
 
 #Show the SQL that created the dept_manager table
-DESCRIBE dept_manager;
+SHOW CREATE TABLE dept_manager;
